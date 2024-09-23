285ef2b06466a7f2f189914487907d8ebba0bb89
Mostafa-YMS <mos_302@yahoo.com>
2024-09-23 17:26:55 +0300

    posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,59 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show update destroy ]
+  before_action :is_authorized, only: %i[ update destroy ]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+
+    render json: @posts
+  end
+
+  # GET /posts/1
+  def show
+    render json: @post
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params.merge(author: current_user))
+
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy!
+  end
+
+  private
+
+  def is_authorized
+    unless current_user.id == @post.author.id
+      render json: { message: "Unauthorized" }, status: :unauthorized
+    end
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-    has_one_attached :image
-    has_secure_password
-    validates :email, uniqueness: true
-    validates_presence_of :name
-    validates_presence_of :email
+  has_one_attached :image
+  has_secure_password
+  validates :email, uniqueness: true
+  validates_presence_of :name
+  validates_presence_of :email
+  has_many :post, dependent: :destroy
 end
