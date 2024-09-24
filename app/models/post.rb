25944bf0d5756c1fbf24edbe1e612baf08d28588
Mostafa-YMS <mos_302@yahoo.com>
2024-09-24 10:30:23 +0300

    delete fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User"
   validates :author, presence: true
-  has_many :tags
-  has_many :comments
+  has_many :tags, dependent: :delete_all
+  has_many :comments, dependent: :delete_all
 end
